c56acde257d74e8b92402df584d77f0c3c45b12f
devMartinsDavid <128499990+FoiLost@users.noreply.github.com>
2024-01-24 10:45:46 -0300

    correção datas

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,20 +7,47 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  yearsProgramation?: number;
-  yearBirth?: number;
-  
-  constructor(
-    private title: Title
-  ) { }
+  yearsProgramation!: number;
+  birthday!: number;
+
+  constructor(private title: Title) { }
 
   ngOnInit(): void {
-    const dataAtual = new Date();
-    
-    this.yearsProgramation = dataAtual.getFullYear() - 2022;
-    this.yearBirth = dataAtual.getFullYear() - 2000;
-    
     this.title.setTitle('DevMartins | Sobre');
+    this.calculateYears();
   }
+
+  calculateYears(): void {
+    const currentDate = new Date();
+    let birthDate = new Date('2000-06-20T17:00:00'); 
+    let startProgramming = new Date('2022-08-08T06:00:00');
+
+    this.birthday = this.calculateAge(birthDate, currentDate);
+    this.yearsProgramation = this.calculateAge(startProgramming, currentDate);
+  }
+
+  calculateAge(startDate: Date, endDate: Date): number {
+    const startYear = startDate.getFullYear();
+    const endYear = endDate.getFullYear();
+    const age = endYear - startYear;
+
+    if (
+      endDate.getMonth() < startDate.getMonth() ||
+      (endDate.getMonth() === startDate.getMonth() && endDate.getDate() < startDate.getDate())
+    ) {
+      return age - 1;
+    }
+
+    return age;
+  }
+
+  formatAge(age: number): string {
+    return age === 1 ? age + ' ano' : age + ' anos';
+  }
+
+  getFormattedAge(type: string): string {
+    return type === 'birthday' ? this.formatAge(this.birthday) : this.formatAge(this.yearsProgramation);
+  }
+
 
 }
