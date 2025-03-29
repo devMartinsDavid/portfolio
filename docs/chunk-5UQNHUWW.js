d1bade852fad420eddbec92fdec8972022b6787f
devMartinsDavid <david_wallison@hotmail.com>
2025-03-29 15:21:23 -0300

    build gitpages

---
+++ docs/chunk-5UQNHUWW.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-N42WJS6A.js";import"./chunk-D42B7NQ6.js";export{a as PortifolioModule};
---
+++ docs/chunk-GOQVD3HA.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OXGNQRCF.js";import"./chunk-XDFKKYLX.js";import"./chunk-D42B7NQ6.js";export{a as HomeModule};
