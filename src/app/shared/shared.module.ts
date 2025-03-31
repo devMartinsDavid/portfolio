48a4d62f100b9b4d3aa8495385fba0c3a54cbac2
devMartinsDavid <david_wallison@hotmail.com>
2025-03-31 17:50:20 -0300

    fixed layout mobile select language

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     LanguageSelectorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   exports: [
     LanguageSelectorComponent
